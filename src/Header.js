d4f61a6f32a6ce90dd88e8bad5ba20fe495807d8
Caleb Martim <calebmartim2113@gmail.com>
2021-06-02 03:58:12 +0000

    mobile-update

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,55 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <section className="header">
-      <div className="titImovel">
-        <h1>Imóvel em Arniqueiras</h1>
-      </div>
-      <div className="img"></div>
-    </section>
+    <>
+      <section className="header">
+        <div className="titImovel">
+          <h1>Imóvel em Arniqueiras</h1>
+          <p>
+            <i>Uma boa casa para você e sua família</i>
+          </p>
+        </div>
+        <div className="img"></div>
+      </section>
+      <section className="divDescriRight">
+        <div className="img-2"></div>
+        <div className="textoDescriRight">
+          <i class="fas fa-home"></i>
+          <ul>
+            <li>200m² de terreno</li>
+            <li>220m² de area construida</li>
+            <li>Condominio 160 com desconto 116 reais</li>
+            <li>4 quartos sendo 2 suites</li>
+          </ul>
+        </div>
+      </section>
+      <section className="divDescriLeft">
+        <div className="textoDescriLeft">
+          <i class="fas fa-bath"></i>
+          <ul>
+            <li>4 banheiros</li>
+            <li>Suite master com banheira</li>
+            <li>Boiler aquecimento água</li>
+            <li>Todas as torneiras e chuveiros com agua quente</li>
+          </ul>
+        </div>
+        <div className="img-3"></div>
+      </section>
+      <section className="divDescriRight">
+        <div className="img-4"></div>
+        <div className="textoDescriRight">
+          <i class="fas fa-couch"></i>
+          <ul>
+            <li>Energia solar para conta de 1mil reais</li>
+            <li>Closet</li>
+            <li>Piscina aquecida</li>
+            <li>Ar condicionado em todos quartos</li>
+            <li>
+              Cabeamento de internet em todos comodos da casa Iluminação em led
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Form } from "./Form";
+// import { Form } from "./Form";
 import { Header } from "./Header";
 
 function App() {
   return (
     <section>
       <Header />
-      <Form />
+      {/* <Form /> */}
     </section>
   );
 }
